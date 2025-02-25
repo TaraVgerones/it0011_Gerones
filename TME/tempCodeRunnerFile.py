cb1bb985cc9a7e61ed80b3a32f61ee5fcf74ec21
TaraVgerones <taravictoriag@gmail.com>
2025-02-25 12:55:02 +0800

    GERONES_TME

---
+++ TME/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+file = open("numbers.txt", 'r') 
+lines = file.readlines()
+
+for i, line in enumerate(lines, start = 1):
+    numbers = list(map(int, line.strip().split(',')))
+    total = sum(numbers)
+    if str(total) == str(total)[::-1]:
+        result = "Palindrome"
+    else:
+        result = "Not a palindrome"
+    print(f"Line {i}: {', '.join(map(str, numbers))} (sum {total}) - {result}")
